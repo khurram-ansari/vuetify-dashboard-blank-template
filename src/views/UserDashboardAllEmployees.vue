596c13d6b0a7c56d210b7d3f5eca6a688fee27fc
khurram-ansari <khury.slm@gmail.com>
2021-12-09 14:34:39 +0500

    Data accurated

--- src/views/UserDashboardAllEmployees.vue
+++ src/views/UserDashboardAllEmployees.vue
@@ -39,6 +39,25 @@
           item-key="name"
           v-model="selectedRows"
         >
+          <template v-slot:item.sno="{ item }">
+            {{ desserts.indexOf(item) + 1 }}
+          </template>
+          <template v-slot:item.name="{ item }">
+            <v-list color="transparent">
+              <v-list-item>
+                <v-list-item-avatar>
+                  <v-img
+                    :alt="`${item.name} avatar`"
+                    :src="getGenderAvatar(item.gender, item.name)"
+                  ></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.name"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </template>
           <template v-slot:header="{ props, on }">
             <thead>
               <tr>
@@ -122,7 +141,7 @@ export default {
     dialog: false,
     dialogDelete: false,
     headers: [
-      { text: "#", value: "protein", sortable: false, filterable: false },
+      { text: "#", value: "sno", sortable: false, filterable: false },
 
       {
         text: "Name",
@@ -131,9 +150,9 @@ export default {
         value: "name",
       },
 
-      { text: "Designation", value: "calories" },
-      { text: "Email", value: "fat" },
-      { text: "Phone", value: "carbs" },
+      { text: "Designation", value: "designation" },
+      { text: "Email", value: "email" },
+      { text: "Phone", value: "phone" },
       { text: "Actions", value: "actions", sortable: false },
     ],
     desserts: [],
@@ -174,77 +193,159 @@ export default {
   },
 
   methods: {
+    getGenderAvatar(gender, name) {
+      if (!gender && !name)
+        return "https://sparktechsolutions.org/wp-content/uploads/2021/03/logoWhiteBg.svg";
+
+      if (gender) {
+        if (gender.toString().toLowerCase() === "male")
+          return `https://avatars.dicebear.com/api/micah/${name}.svg?options[facialHairProbability]=40&options[hair]=fonze&options[baseColor]=white&options[mouth]=smile&options[earringsProbability]=0`;
+        return `https://avatars.dicebear.com/api/micah/${name}.svg?options[facialHairProbability]=0&options[hair]=full&options[baseColor]=white&options[mouth]=smile&options[earringsProbability]=40`;
+      }
+      return `https://avatars.dicebear.com/api/initials/${name}.svg`;
+    },
+
     initialize() {
       this.desserts = [
         {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
+          name: "Geno Wilby",
+          email: "gwilby0@soundcloud.com",
+          gender: "Male",
+          designation: "Developer I",
+          phone: "474-476-7259",
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
+          name: "Niko Mattacks",
+          email: "nmattacks1@webnode.com",
+          gender: "Female",
+          designation: "Mechanical Systems Engineer",
+          phone: "964-103-3518",
         },
         {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
+          name: "Stu Andrieux",
+          email: "sandrieux2@merriam-webster.com",
+          gender: "Male",
+          designation: "Human Resources Manager",
+          phone: "921-490-1015",
         },
         {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
+          name: "Bria Limpkin",
+          email: "blimpkin3@cmu.edu",
+          gender: "Female",
+          designation: "Quality Engineer",
+          phone: "923-398-9988",
         },
         {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
+          name: "Oona Bartke",
+          email: "obartke4@chronoengine.com",
+          gender: "Male",
+          designation: "Information Systems Manager",
+          phone: "728-518-5218",
         },
         {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
+          name: "Chelsey Kunrad",
+          email: "ckunrad5@netscape.com",
+          gender: "Female",
+          designation: "Programmer Analyst I",
+          phone: "366-384-0008",
         },
         {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
+          name: "Basilio Joblin",
+          email: "bjoblin6@dropbox.com",
+          gender: "Female",
+          designation: "Media Manager IV",
+          phone: "964-958-4118",
         },
         {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
+          name: "Leta Powlesland",
+          email: "lpowlesland7@fotki.com",
+          gender: "Female",
+          designation: "Geological Engineer",
+          phone: "294-709-2226",
         },
         {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
+          name: "Granger Shawyer",
+          email: "gshawyer8@aboutads.info",
+          gender: "Female",
+          designation: "Design Engineer",
+          phone: "894-229-1968",
         },
         {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
+          name: "Kalila Baccup",
+          email: "kbaccup9@csmonitor.com",
+          gender: "Female",
+          designation: "Pharmacist",
+          phone: "402-834-0723",
+        },
+        {
+          name: "Lucille Mitrikhin",
+          email: "lmitrikhina@hostgator.com",
+          gender: "Male",
+          designation: "Office Assistant I",
+          phone: "592-359-9058",
+        },
+        {
+          name: "Tabby Higgonet",
+          email: "thiggonetb@tamu.edu",
+          gender: "Male",
+          designation: "Programmer II",
+          phone: "894-355-6801",
+        },
+        {
+          name: "Tammi Chuney",
+          email: "tchuneyc@woothemes.com",
+          gender: "Female",
+          designation: "Nurse Practicioner",
+          phone: "823-154-6640",
+        },
+        {
+          name: "Othello Gerram",
+          email: "ogerramd@parallels.com",
+          gender: "Female",
+          designation: "Software Consultant",
+          phone: "781-264-9535",
+        },
+        {
+          name: "Caressa Redmire",
+          email: "credmiree@naver.com",
+          gender: "Male",
+          designation: "Engineer I",
+          phone: "474-235-3953",
+        },
+        {
+          name: "Nadiya Ebanks",
+          email: "nebanksf@washington.edu",
+          gender: "Male",
+          designation: "Internal Auditor",
+          phone: "575-881-4169",
+        },
+        {
+          name: "Trenna Newton",
+          email: "tnewtong@samsung.com",
+          gender: "Male",
+          designation: "Compensation Analyst",
+          phone: "707-423-0501",
+        },
+        {
+          name: "Sabina Chiechio",
+          email: "schiechioh@nih.gov",
+          gender: "Female",
+          designation: "Chemical Engineer",
+          phone: "424-120-2921",
+        },
+        {
+          name: "Patti Daynter",
+          email: "pdaynteri@dedecms.com",
+          gender: "Male",
+          designation: "Staff Accountant III",
+          phone: "850-449-0522",
+        },
+        {
+          name: "Alfred Hamon",
+          email: "ahamonj@netlog.com",
+          gender: "Male",
+          designation: "Food Chemist",
+          phone: "809-488-3818",
         },
       ];
     },
